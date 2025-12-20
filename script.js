d0894e4467f3005b0d39f97e0dab5681a3cdf477
MartynovychSerhii <131612238+MartynovychSerhii@users.noreply.github.com>
2025-12-20 21:37:33 +0200

    Added copy buttons Added Javascript interactions on copy buttons Added new style page

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function copyToClipboard(elementId, btnElement) {
+    const element = document.getElementById(elementId);
+    if (!element) return;
+
+    // 1. Беремо посилання або текст
+    let content = element.getAttribute('href') || element.innerText;
+
+    // 2. Очищуємо від префіксів mailto: та tel:
+    // Це зробить так, що копіюватиметься тільки чистий номер або пошта
+    content = content.replace('mailto:', '').replace('tel:', '');
+
+    navigator.clipboard.writeText(content).then(() => {
+        const icon = btnElement.querySelector('i');
+        const originalClass = icon.className;
+
+        icon.className = 'fa-solid fa-check';
+        btnElement.style.color = '#2ecc71';
+
+        setTimeout(() => {
+            icon.className = originalClass;
+            btnElement.style.color = '';
+        }, 2000);
+    });
+}
